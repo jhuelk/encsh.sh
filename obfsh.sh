c96e03ed1914736240c7b5e7883aa9bb3dc25ca3
jhuelk <jhuelk@github>
2020-12-15 21:12:56 -0300

    more crap

--- obfsh.sh
+++ obfsh.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -u
 
-CIPHER="aes-128-cbc"
-PBKDF2=$(openssl version | awk '$2 !~ /(^0\.)|(^1\.(0\.|1\.0))/ { print "-pbkdf2" }');
 INT="/bin/bash" # default interpreter
 
 if [ "$#" -lt 1 ]; then
@@ -20,7 +18,7 @@ if [ ! -f "${1}" ]; then
 fi
 
 P=$(dirname "${1}")
-O=${P}/$(basename "${1}" ".${1##*.}")_enc.sh
+O=${P}/$(basename "${1}" ".${1##*.}")_obf.sh
 
 if [ -f "${O}" ]; then
 	echo -n "${O} exists. Overwrite? (Y/n) "
@@ -30,10 +28,10 @@ if [ -f "${O}" ]; then
 	fi
 fi
 
-C=$(gzip < "${1}" | openssl ${CIPHER} -a -salt ${PBKDF2})
+C=$(gzip < "${1}" | base64)
 
 if [ $? -ne 0 ];then
-	echo "openssl error"
+	echo "error"
 	exit
 fi
 
@@ -45,7 +43,7 @@ ${C}\n\
 '\n\
 N=\"/dev/null\";\
 W=\$(which which);\
-C=\$(\${W} openssl);\
+C=\$(\${W} base64);\
 Z=\$(\${W} gunzip);\
 D=\$(\${W} dirname);\
 M=\$(\${W} mktemp);\
@@ -53,7 +51,7 @@ if \${W} shred 1>\${N}; then R=\"\$(\${W} shred) -u\"; else R=\$(\${W} rm); fi;\
 P=\$(\${D} \"\${0}\");\
 O=\$(\${M} -p \"\${P}\");\
 c(){ trap - \${G}; \${R} \"\${O}\" 2>\${N}; };\
-echo \"\${S}\" | \${C} ${CIPHER} -d -a -salt ${PBKDF2} 2>\${N} | \${Z} > \"\${O}\" 2>\${N}; ${INT} \"\${O}\" \${*}\
+echo \"\${S}\" | \${C} -d 2>\${N} | \${Z} > \"\${O}\" 2>\${N}; ${INT} \"\${O}\" \${*}\
 "
 
 echo -e "${H}" > "${O}"
